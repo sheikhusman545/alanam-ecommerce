aa9c70172f0dd1f05b543119b8578c90aaf17697
Muhammad Usman <sheikhusman545@gmail.com>
2024-10-14 03:55:53 +0500

    autoslider

--- src/app/homepage-2/homepage-2.page.ts
+++ src/app/homepage-2/homepage-2.page.ts
@@ -14,6 +14,7 @@ import { forkJoin } from 'rxjs';
 })
 export class Homepage2Page implements OnInit {
   @ViewChild('swiper') swiperRef: ElementRef | undefined;
+
   swiper?: Swiper;
   featuredProducts: any[] = [];
   newProduct: any[] = [];
