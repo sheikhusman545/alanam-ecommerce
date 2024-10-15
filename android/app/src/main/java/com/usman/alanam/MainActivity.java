362cabcac5d3f4fa9adc132447159d6cf43c1747
Muhammad Usman <sheikhusman545@gmail.com>
2024-10-15 17:52:12 +0500

    Splash Screens

---
+++ android/app/src/main/java/com/usman/alanam/MainActivity.java
@@ -0,0 +1,5 @@
+package com.usman.alanam;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.usman.alanam',
   appName: 'alanam',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 500,
+      launchAutoHide: true,
+      launchFadeOutDuration: 500,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+    },
+  },
 };
 
 export default config;
