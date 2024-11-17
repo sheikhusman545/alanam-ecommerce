70ed84ea4f1f48e6dd2946e88a1e57c453973206
Muhammad Usman <sheikhusman545@gmail.com>
2024-11-17 16:23:22 +0500

    android last build
    last build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.shop.alanaam',
+  appId: 'com.usman.alanam',
   appName: 'AL ANAAM',
   webDir: 'www',
   plugins: {
